c336af85068b2253a601980693771e61ee78e381
Edward Needham <edwardneedham123@gmail.com>
2020-03-06 17:15:57 +0000

    added todos reducer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,10 +2,15 @@ import axios from 'axios';
 import { Dispatch } from 'redux';
 import { ActionTypes } from './types';
 
-interface Todo {
+export interface Todo {
     id: number;
     title: string;
     completed: boolean;
+}
+
+export interface FetchTodosAction {
+    type: ActionTypes.fetchTodos;
+    payload: Todo[];
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
@@ -14,7 +19,7 @@ export const fetchTodos = () => {
     return async (dispatch: Dispatch) => {
         const response = await axios.get<Todo[]>(url);
 
-        dispatch({
+        dispatch<FetchTodosAction>({
             type: ActionTypes.fetchTodos,
             payload: response.data
         });
