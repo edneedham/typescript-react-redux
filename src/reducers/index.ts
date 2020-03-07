14f2aea4e21e11ff2c52d009508118dec50507c1
Edward Needham <edwardneedham123@gmail.com>
2020-03-07 11:13:39 +0000

    added event handler for fetching todos and a function to render todos in a list to the page

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions/index';
+export interface StoreState {
+    todos: Todo[]
+}
 
-export const reducers = combineReducers({
-    counter: () => 1
+// Makes sure that the reducers have the correctly formatted todos else typescript returns error
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducer
 });
